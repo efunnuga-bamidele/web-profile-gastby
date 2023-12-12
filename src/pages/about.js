68acaa1336c2fba2e8b8b963adde4cf798a84363
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-12-12 07:29:53 +0100

    Initial Commit

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>
+        Lorem pariatur minim cillum tempor ea. Duis ullamco velit id nisi Lorem
+        exercitation mollit eu aliquip ex consectetur. Cupidatat magna aliquip
+        magna mollit sit fugiat minim commodo ea officia ipsum reprehenderit
+        excepteur. Excepteur incididunt esse Lorem in minim duis. Nulla minim
+        consequat nisi eiusmod laboris eiusmod elit ea ipsum sunt culpa est
+        proident minim. Sit ipsum commodo enim eiusmod labore Lorem ullamco
+        Lorem sunt non proident ea culpa.
+      </p>
+      <p>
+        Lorem pariatur minim cillum tempor ea. Duis ullamco velit id nisi Lorem
+        exercitation mollit eu aliquip ex consectetur. Cupidatat magna aliquip
+        magna mollit sit fugiat minim commodo ea officia ipsum reprehenderit
+        excepteur. Excepteur incididunt esse Lorem in minim duis. Nulla minim
+        consequat nisi eiusmod laboris eiusmod elit ea ipsum sunt culpa est
+        proident minim. Sit ipsum commodo enim eiusmod labore Lorem ullamco
+        Lorem sunt non proident ea culpa.
+      </p>
+      <p>
+        Lorem pariatur minim cillum tempor ea. Duis ullamco velit id nisi Lorem
+        exercitation mollit eu aliquip ex consectetur. Cupidatat magna aliquip
+        magna mollit sit fugiat minim commodo ea officia ipsum reprehenderit
+        excepteur. Excepteur incididunt esse Lorem in minim duis. Nulla minim
+        consequat nisi eiusmod laboris eiusmod elit ea ipsum sunt culpa est
+        proident minim. Sit ipsum commodo enim eiusmod labore Lorem ullamco
+        Lorem sunt non proident ea culpa.
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import * as React from "react"
+import * as React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <h2>Design</h2>
+      <h4>Home Page</h4>
+      <p>
+        Laboris exercitation qui quis esse anim minim culpa minim fugiat elit
+        duis aliqua qui. Laboris adipisicing cupidatat nisi duis nulla culpa
+        nisi esse ad ad. Elit ad do tempor incididunt non id Lorem eiusmod
+        aliquip tempor et ullamco do. Ea fugiat amet qui duis anim. Cillum
+        laboris excepteur ea occaecat proident veniam tempor magna.
+      </p>
+    </section>
+  );
 }
